4a222eab99a23e6169b93613012a0fdbd72c4dc2
Jordan Gunn <gunnjordanb@gmail.com>
2021-02-13 17:15:51 -0600

    Debugged interface/web app

--- rememberly/rememberly_web.py
+++ rememberly/rememberly_web.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, request, redirect
-import sys
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def startpage():
+        if request.method == 'POST':
+                print(request.form['inputText'])
         return render_template('interface.html')
-
-@app.route('/data')
-def get_HTML_data():
-        text = request.form['textInput']
-        sys.stderr.write(text)
-        return redirect('/')
