34446f9aeeead962c34cacd3b6cc94235ce740a4
spectraldoy <littleguyisfum@gmail.com>
2021-02-13 12:52:34 +0530

    updated text initial text similarity calc

--- 01_LandscapeModel/textsimilarity.py
+++ 01_LandscapeModel/textsimilarity.py
@@ -22,6 +22,7 @@ class TextSimilarity(torch.nn.Module):
     Computes embeddings for a pair of given sentences and calculates the cosine
     similarity between them
     """
+
     def __init__(self, sbert_model_name):
         """
         sbert_model_name: sbert model to use, I think "stsb-roberta-base" is good
@@ -30,29 +31,24 @@ class TextSimilarity(torch.nn.Module):
         self.model = SentenceTransformer(sbert_model_name)
         self.to(dev)
 
-    @staticmethod
-    def measure(embedded_reading_cycle):
-        """
-        Computes cosine similarity between all text unit embeddings in a reading cycle
-        reading_cycle: list of text unit embeddings
-        """
-        # no customizable metric - only cosine, otherwise 1 - x doesn't make sense
-        similarities = 1 - scipy.spatial.distance.pdist(embedded_reading_cycle, metric="cosine")
-        # convert to torch tensors
-        return torch.tensor(similarities)
-
     def forward(self, reading_cycles):
         """
         Computes cosine similarities between n sentences as a matrix
+        designed to take in a list of reading_cycles, not a list of text units
+        from the text segmentation process
         reading_cycles: list of lists of text unit strings
+
+        Returns: S, the initial similarity matrix over all text units
         """
-        # embeds is the list of text_unit embeddings for each reading cycle of shape
-        # (num_reading_cycles, num_text_units, embed_dim)
-        # where num_text_units varies per reading cycle, and embed_dim = 768 as per BERT
-        embeds = [self.model.encode(reading_cycle, convert_to_tensor=True) for reading_cycle in reading_cycles]
-        # given n text units in a given reading_cycle
-        # pdist will compute n * (n - 1) // 2 similarities, so measures is a list of shape
-        # of shape (num_reading_cycles, num_text_units * (num_text_units - 1) // 2)
-        measures = [self.measure(emb_reading_cycle) for emb_reading_cycle in embeds]
+        # expand the reading cycles into a list of text units
+        text_unit_list = [text_unit for reading_cycle in reading_cycles for text_unit in reading_cycle]
+
+        # embeds is the tensor of text_unit embeddings over all text_units in the reading cycle
+        # of shape (n, embed_dim), embed_dim=768 in BERT, n = num_text_units
+        embeds = self.model.encode(text_unit_list, convert_to_tensor=True)
+
+        # cosine similarities between every text unit of shape (n * (n - 1) // 2, )
+        S = 1 - scipy.spatial.distance.pdist(embeds, metric="cosine")
+
         # return similarity measures and embeddings
-        return measures, embeds
+        return torch.tensor(S), embeds
